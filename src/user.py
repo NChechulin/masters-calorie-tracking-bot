2dabada70f82014806041e6af075cfe098817cfd
Nikolay Chechulin <nchechulin@protonmail.com>
2025-01-22 15:34:25 +0300

    separate user state and user model

--- src/user.py
+++ src/user.py
@@ -4,9 +4,28 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 @dataclass
-class UserModel(StatesGroup):
+class UserState(StatesGroup):
     activity: State
     age: State
     city: State
     height: State
     weight: State
+
+
+@dataclass
+class UserModel:
+    activity: float
+    age: float
+    city: str
+    height: float
+    weight: float
+
+    def __post_init__(self) -> None:
+        if self.activity < 0 or self.activity >= 24 * 60:
+            raise ValueError("Activity can not be < 0 or > 24 hrs")
+        if self.age <= 0:
+            raise ValueError("Age can not be negative")
+        if self.height <= 0:
+            raise ValueError("Height can not be negative")
+        if self.weight <= 0:
+            raise ValueError("Weight can not be negative")
