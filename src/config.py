ccad268b27a8af2851a69ce6a31b4de8a634ed6f
Nikolay Chechulin <nchechulin@protonmail.com>
2025-01-22 15:49:34 +0300

    calculate requirements in post_init instead of calculating them outsude

--- src/config.py
+++ src/config.py
@@ -9,6 +9,8 @@ from dotenv import load_dotenv
 @dataclass(init=False)
 class Config:
     BOT_TOKEN: str
+    WEATHER_TOKEN: str
+    WEATHER_API_BASE: str
 
     def __init__(self, dotenv_path: Path = Path(".env")) -> None:
         load_dotenv(dotenv_path)
@@ -17,4 +19,14 @@ class Config:
                 raise KeyError("Config does not contain 'BOT_TOKEN' variable")
             case token:
                 self.BOT_TOKEN = token
+        match getenv("WEATHER_TOKEN"):
+            case None:
+                raise KeyError("Config does not contain 'WEATHER_TOKEN' variable")
+            case token:
+                self.WEATHER_TOKEN = token
+        match getenv("WEATHER_API_BASE"):
+            case None:
+                raise KeyError("Config does not contain 'WEATHER_API_BASE' variable")
+            case url:
+                self.WEATHER_API_BASE = url
         logging.info(f"Read config: {self}")
--- src/user.py
+++ src/user.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
 
+import requests
 from aiogram.fsm.state import State, StatesGroup
+
+from config import Config
+
+config = Config()
 
 
 @dataclass
@@ -19,13 +24,33 @@ class UserModel:
     city: str
     height: float
     weight: float
-    water_goal: float = 0
-    calorie_goal: float = 0
+    water_goal: int = 0
+    calorie_goal: int = 0
     logged_water: float = 0
     logged_calories: float = 0
     burnt_calories: float = 0
 
+    def get_temperature(self) -> float:
+        response = requests.get(
+            config.WEATHER_API_BASE,
+            params={
+                "q": self.city,
+                "appid": config.WEATHER_TOKEN,
+                "units": "metric",
+            },
+            timeout=5,
+        )
+
+        if response.status_code == 200:
+            temp = float(response.json()["main"]["temp"])
+            return temp
+        else:
+            raise ValueError(
+                "Incorrect city might have been entered. API response: {response}"
+            )
+
     def __post_init__(self) -> None:
+        # data validation
         if self.activity < 0 or self.activity >= 24 * 60:
             raise ValueError("Activity can not be < 0 or > 24 hrs")
         if self.age <= 0:
@@ -34,3 +59,13 @@ class UserModel:
             raise ValueError("Height can not be negative")
         if self.weight <= 0:
             raise ValueError("Weight can not be negative")
+
+        # requirements calculation
+        self.water_goal = int(self.weight * 30 + 500 * (self.activity / 30))
+        temp = self.get_temperature()
+        if temp > 25:
+            self.water_goal += 500
+
+        self.calorie_goal = int(10 * self.weight + 6.25 * self.height - 5 * self.age)
+        if self.activity > 50:
+            self.calorie_goal += 300
