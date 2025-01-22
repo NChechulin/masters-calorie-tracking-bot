2ee416c89c790cea1a758cbe090e2bac4bac3ca7
Nikolay Chechulin <nchechulin@protonmail.com>
2025-01-22 15:02:30 +0300

    create a custom logger

---
+++ src/logger.py
@@ -0,0 +1,15 @@
+from typing import Any, Callable
+
+from aiogram import BaseMiddleware
+from aiogram.types import Message
+
+
+class BotLogger(BaseMiddleware):
+    async def __call__(  # type: ignore[override]
+        self,
+        handler: Callable,
+        event: Message,
+        data: dict[str, Any],
+    ) -> Any:
+        print(f"Received a message: {event.text}")
+        return await handler(event, data)
